caf48ae9a6dfb3697a36a536e3b997ff2e4c5325
Jose Pulgar <jpulgar@searshc.com>
2014-05-14 17:06:15 -0500

    added concat, annotate and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     nodemon = require('gulp-nodemon'),
+    ngAnnotate = require('gulp-ng-annotate'),
     paths = {
         scripts: ['gulpfile.js', 'server.js', 'config/*.js', 'models/*.js', 'routes/*.js', 'utilities/*.js']
 };
@@ -12,9 +15,17 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter(stylish));
 });
 
+gulp.task('concat', function() {
+    gulp.src(['./public/js/app.js','./public/js/controllers.js','./public/js/directives.js','./public/js/services.js'])
+        .pipe(concat('app.all.min.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/'));
+});
+
 gulp.task('develop', function () {
     nodemon({ script: 'server.js', ext: 'html js' })
         .on('change', ['lint']);
 });
 
-//gulp.task('default', ['lint']);
+gulp.task('default', ['lint', 'concat']);
